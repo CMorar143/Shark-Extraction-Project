12726c890ecb29ff37519524f01f289d6fd177b4
CMorar143 <cian@morar.org>
2019-10-05 15:34:47 +0100

    extracting the channels from YUV and HSV

--- Assessment1.py
+++ Assessment1.py
@@ -14,15 +14,29 @@ I = cv2.imread(f)
 ######### Colourspaces #########
 
 # Converting to different colour spaces:
-RGB = cv2.cvtColor(I, cv2.COLOR_BGR2RGB)
+# RGB = cv2.cvtColor(I, cv2.COLOR_BGR2RGB)
 HSV = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
 YUV = cv2.cvtColor(I, cv2.COLOR_BGR2YUV)
-G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+# G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+
+Y = YUV[:,:,0]
+U = YUV[:,:,1]
+yuV = YUV[:,:,2]
+
+H = HSV[:,:,0]
+S = HSV[:,:,1]
+hsV = HSV[:,:,2]
+
 
 # Showing each image on the screen in a different window (OpenCV):
-cv2.imshow("Original", I)
-cv2.imshow("HSV", HSV)
-cv2.imshow("YUV", YUV)
-cv2.imshow("Grayscale", G)
+# cv2.imshow("Original", I)
 cv2.imshow("Y", Y)
+cv2.imshow("U", U)
+cv2.imshow("yuV", yuV)
+
+cv2.imshow("H", H)
+cv2.imshow("S", S)
+cv2.imshow("hsV", hsV)
+
+
 key = cv2.waitKey(0)
