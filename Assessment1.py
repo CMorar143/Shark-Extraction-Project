1693f1fe0181c120d68004b7e95eee39775a40a2
CMorar143 <cian@morar.org>
2019-10-12 17:51:09 +0100

    trying to figure out how to convert background to white

--- Assessment1.py
+++ Assessment1.py
@@ -11,11 +11,11 @@ from matplotlib import image as image
 # Opening an image using a File Open dialog:
 # F = gui.fileopenbox()
 # I = cv2.imread(F)
-I = cv2.imread("Shark_2.PNG")
+I = cv2.imread("Shark_1.PNG")
 
 h, w, d = I.shape
-# blue_image = I.copy()
-# blue_image[:,:] = (255,0,0)
+blue_image = I.copy()
+blue_image[:,:] = (255,0,0)
 
 
 # Converting the colour space to YUV and extracting the Luminance (Y) From the image:
@@ -23,7 +23,6 @@ YUV = cv2.cvtColor(I, cv2.COLOR_BGR2YUV)
 
 # Converting to different colour spaces:
 HSV = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
-G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
 
 # Extract the Y, U, and V from the YUV image:
 Y = YUV[:,:,0]
@@ -37,7 +36,7 @@ HV = HSV[:,:,2]
 
 # Using the Contrast Limited Adaptive Histogram Equalization class to enhance the contrast
 # Create the CLAHE object and set the clip limit and tile grid size:
-CLAHE = cv2.createCLAHE(clipLimit = 4.5, tileGridSize = (2,2))
+CLAHE = cv2.createCLAHE(clipLimit = 4.5, tileGridSize = (3,3))
 
 # Enchance contrast for the luminance (Y) channel in the image
 YE = CLAHE.apply(Y)
@@ -51,14 +50,23 @@ HVE = CLAHE.apply(HV)
 Enchanced_YUV = cv2.merge((YE,U,V))
 Enchanced_HSV = cv2.merge((H,S,HVE))
 
-range_lower = np.asarray([0,0,150])
-range_higher = np.asarray([255,255,185])
+# range_lower = np.asarray([0,0,150])
+# range_higher = np.asarray([255,255,185])
 
-masked_range = cv2.inRange(HSV, range_lower, range_higher)
+# masked_range = cv2.inRange(HSV, range_lower, range_higher)
 
 # Convert the new YUV image back to the original BGR colour space
 Enchanced_BGR_YUV = cv2.cvtColor(Enchanced_YUV, cv2.COLOR_YUV2BGR)
 Enchanced_BGR_HSV = cv2.cvtColor(Enchanced_HSV, cv2.COLOR_HSV2BGR)
+Enchanced_YUV_HSV = cv2.cvtColor(Enchanced_BGR_HSV, cv2.COLOR_BGR2YUV)
+
+# Enchanced_YUV_B = Enchanced_BGR_YUV[:,:,0]
+# Enchanced_YUV_G = Enchanced_BGR_YUV[:,:,1]
+# Enchanced_YUV_R = Enchanced_BGR_YUV[:,:,2]
+
+Enchanced_HSV_B = Enchanced_BGR_HSV[:,:,0]
+# Enchanced_HSV_G = Enchanced_BGR_HSV[:,:,1]
+# Enchanced_HSV_R = Enchanced_BGR_HSV[:,:,2]
 
 # finalhsv = cv2.fastNlMeansDenoisingColored(Enchanced_BGR_HSV,None,20,10,7,20)
 # finalyuv = cv2.fastNlMeansDenoisingColored(Enchanced_BGR_YUV,None,20,10,7,20)
@@ -68,18 +76,22 @@ Enchanced_BGR_HSV = cv2.cvtColor(Enchanced_HSV, cv2.COLOR_HSV2BGR)
 
 # BG = Enchanced_BGR[:,:,0]
 
-# cv2.imshow("Enchanced ImageY", Enchanced_BGR_YUV)
-# cv2.imshow("Enchanced ImageH", Enchanced_BGR_HSV)
-cv2.imshow("ranged HSV", masked_range)
-# cv2.imshow("HSV", HSV)
-cv2.imshow("Y", Y)
-cv2.imshow("U", U)
-# cv2.imshow("finalhsv", finalhsv)
-# cv2.imshow("finalyuv", finalyuv)
-cv2.imshow("HV", HV)
-cv2.imshow("HVE", HVE)
+# cv2.imshow("b",I[:,:,0])
+# cv2.imshow("Enchanced_YUV_B", Enchanced_YUV_B)
+# cv2.imshow("Enchanced_YUV_G", Enchanced_YUV_G)
 
-key = cv2.waitKey(0)
+# cv2.imshow("Enchanced_HSV_B", Enchanced_HSV_B)
+# cv2.imshow("Enchanced_HSV_G", Enchanced_HSV_G)
+# cv2.imshow("Enchanced ImageH", Enchanced_BGR_HSV)
+# cv2.imshow("HV", HV)
+# cv2.imshow("Y", Y)
+# cv2.imshow("U", U)
+# cv2.imshow("final_image", final_image)
+# cv2.imshow("finalyuv", finalyuv)
+# cv2.imshow("HV2", HV2)
+# cv2.imshow("HVE", HVE)
+
+# key = cv2.waitKey(0)
 
 
 
@@ -102,17 +114,17 @@ ax3.imshow(HVE, cmap='gray')
 ax4 = fig.add_subplot(gs[1,1])
 ax4.hist(HVE.ravel(), bins=256, range=[0,256])
 
-# # ax5 = fig.add_subplot(gs[2,0])
-# # ax5.imshow(Enchanced_BGR_YUV, cmap='gray')
+# ax5 = fig.add_subplot(gs[2,0])
+# ax5.imshow(l_channel, cmap='gray')
 
-# # ax6 = fig.add_subplot(gs[2,1])
-# # ax6.hist(Enchanced_BGR_YUV.ravel(), bins=256, range=[0,256])
+# ax6 = fig.add_subplot(gs[2,1])
+# ax6.hist(l_channel.ravel(), bins=256, range=[0,256])
 
-# # ax7 = fig.add_subplot(gs[3,0])
-# # ax7.imshow(Enchanced_BGR_HSV, cmap='gray')
+# ax7 = fig.add_subplot(gs[3,0])
+# ax7.imshow(Enchanced_BGR_HSV, cmap='gray')
 
-# # ax8 = fig.add_subplot(gs[3,1])
-# # ax8.hist(Enchanced_BGR_HSV.ravel(), bins=256, range=[0,256])
+# ax8 = fig.add_subplot(gs[3,1])
+# ax8.hist(Enchanced_BGR_HSV.ravel(), bins=256, range=[0,256])
 
 plt.show()
 cv2.waitKey(0)
