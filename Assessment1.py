3d4c7b5e59341bc03e07bcc0e33828a7291170a1
CMorar143 <cian@morar.org>
2019-10-20 23:16:25 +0100

    cleaning comments

--- Assessment1.py
+++ Assessment1.py
@@ -1,94 +1,104 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Program to Detect and Extract a Shark From an Image.						  #
+# Author: Cian Morar 														  #
+# Date: October 2019														  #
+# 																			  #
+# User selects an image. 													  #
+# 																			  #
+# Convert image to the YUV color space.										  #
+# 																			  #
+# Extract and enhance the contrast of the Y and U channels.					  #
+# 																			  #
+# Threshold the U channel to create a mask (for background subtraction).	  #
+# 																			  #
+# Using the enhanced Y channel, convert YUV image back to BGR.				  #
+# 																			  #
+# Apply the mask and convert back to YUV.									  #
+# 																			  #
+# Extract the U channel from the enhanced YUV image.					 	  #
+# 																			  #
+# Create another mask.														  #
+# 																			  #
+# Apply the final mask (to remove some noise from the image).				  #
+# 																			  #
+# Display the extracted shark.												  #
+# 																			  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
 # import the necessary packages:
 import cv2
 import easygui as gui
 
-# Function to apply a threshold to an image.
-# It is used to remove the background
-def ApplyMask(threshold):
-	# Extract the shark using the mask created by the thresholding
-	extracted_sharkB = cv2.bitwise_or(threshold, Enchanced_YUV_B)
-	extracted_sharkG = cv2.bitwise_or(threshold, Enchanced_YUV_G)
-	extracted_sharkR = cv2.bitwise_or(threshold, Enchanced_YUV_R)
+# Messages for the Graphical User Interface:
+opening_message = "This Application Allows You to Extract a Shark From an Image.\n\n\n"
+instructions = "Please Choose The Image That You Would Like to Use."
+closing_message = "\tHere's The Shark!\n\tWould You Like to Select Another Picture?"
+choices = ["Yes", "No"]
+final_message = "\tHave a great day!"
 
-	# Merge the channels
+# Function to apply a threshold to an image
+# It is used to remove the background:
+def ApplyMask(threshold):
+	# Apply the mask to each color channel in the image:
+	extracted_sharkB = cv2.bitwise_or(threshold, Enchanced_B)
+	extracted_sharkG = cv2.bitwise_or(threshold, Enchanced_G)
+	extracted_sharkR = cv2.bitwise_or(threshold, Enchanced_R)
+
 	merged = cv2.merge((extracted_sharkB, extracted_sharkG, extracted_sharkR))
 
 	return merged
 
-# Messages for the Graphical User Interface
-closing_message = "\tHere's The Shark!\n\tWould You Like to Select Another Picture?"
-choices = ["Yes", "No"]
-opening_message = "This Application Allows You to Extract a Shark From an Image.\n\n\n"
-instructions = "Please Choose The Image That You Would Like to Use."
-
-# Repeat until the user exits
+# Repeat until the user chooses to exit:
 while 1:
-	# Introduction and instructions
 	gui.msgbox(opening_message + instructions, "Hello!")
 
-	# Opening an image using a File Open dialog:
 	F = gui.fileopenbox()
 	I = cv2.imread(F)
 
-	# Converting the colour space to YUV and extracting the Luminance (Y) From the image:
 	YUV = cv2.cvtColor(I, cv2.COLOR_BGR2YUV)
 
-	# Extract the Y, U, and V from the YUV image:
 	Y, U, V = cv2.split(YUV)
 
 	# Using the Contrast Limited Adaptive Histogram Equalization class to enhance the contrast
 	# Create the CLAHE object and set the clip limit and tile grid size:
 	CLAHE = cv2.createCLAHE(clipLimit = 4.5, tileGridSize = (3,3))
 
-	# Enchance contrast for the luminance (Y) channel in the image
-	# This is done to improve definition in the image
-	YE = CLAHE.apply(Y)
+	# This is done to improve definition in the image:
+	Enhanced_Y = CLAHE.apply(Y)
 
-	# Enhance the contrast for the U channel
-	# This is used to remove the background
-	UE = CLAHE.apply(U)
+	# This is used to remove the background:
+	Enhanced_U = CLAHE.apply(U)
 
-	# use thresholding to create the mask
-	_, th1U = cv2.threshold(UE, 176, 255, cv2.THRESH_TRUNC)
-	th2U = cv2.adaptiveThreshold(th1U, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 275, 2)
+	# Create initial mask to remove the background:
+	_, Threshold = cv2.threshold(Enhanced_U, 176, 255, cv2.THRESH_TRUNC)
+	Adaptive_Threshold = cv2.adaptiveThreshold(Threshold, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 275, 2)
 
-	# Merge the enhanced luminance with the original U and V values to form a full YUV image
-	Enchanced_YUV = cv2.merge((YE,U,V))
+	Enchanced_YUV = cv2.merge((Enhanced_Y,U,V))
+	Enchanced_BGR = cv2.cvtColor(Enchanced_YUV, cv2.COLOR_YUV2BGR)
+	Enchanced_B, Enchanced_G, Enchanced_R = cv2.split(Enchanced_BGR)
 
-	# Convert the new YUV image back to the BGR colour space
-	Enchanced_BGR_YUV = cv2.cvtColor(Enchanced_YUV, cv2.COLOR_YUV2BGR)
+	Removed_Background = ApplyMask(Adaptive_Threshold)
 
-	# Extract the colour spaces in order to apply the mask to each channel
-	Enchanced_YUV_B, Enchanced_YUV_G, Enchanced_YUV_R = cv2.split(Enchanced_BGR_YUV)
+	# Create the final mask to clean more of the noise:
+	Enhanced_YUV2 = cv2.cvtColor(Removed_Background, cv2.COLOR_BGR2YUV)
+	u = Enhanced_YUV2[:,:,1]
 
-	f = ApplyMask(th2U)
-
-	YUV2 = cv2.cvtColor(f, cv2.COLOR_BGR2YUV)
-
-	u = YUV2[:,:,1]
-
-	# Create another mask to remove more noise from the image
-	minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(u)
-	print(minVal)
-	print(maxVal)
-	print(minLoc)
-	print(maxLoc)
-
+	_, maxVal, _, _ = cv2.minMaxLoc(u)
 	masked_range = cv2.inRange(u, 0, maxVal-3)
-	m = cv2.bitwise_xor(masked_range, th2U)
-	m = cv2.bitwise_not(m)
+	final_mask = cv2.bitwise_xor(masked_range, Adaptive_Threshold)
+	final_mask = cv2.bitwise_not(final_mask)
 
-	final_image = ApplyMask(m)
+	final_image = ApplyMask(final_mask)
 
 	cv2.imwrite("./final_image.png", final_image)
 
 	reply = gui.buttonbox(closing_message, image = "./final_image.png", choices = choices)
 
-	if reply == 'No':
-
+	# Close the application if they click no or if they try to close the window
+	if reply == 'No' or reply != 'Yes':
+		gui.msgbox(final_message, title = "Thanks!")
 		exit(1)
-
-
 
 
 
